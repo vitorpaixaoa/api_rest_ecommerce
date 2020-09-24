ff4fc27831592c4b196cd780bb2eda26756fec12
Alan Vítor Paixão Almeida <vitorpaixaoa.loubi@gmail.com>
2020-09-24 15:13:57 -0300

    Modelo de Usuario

---
+++ models/usuarios.js
@@ -0,0 +1,90 @@
+const mongoose = require("mongoose"),
+    Schema = mongoose.Schem;
+
+const uniqueValidator = require("mongoose-unique-validator");
+const crypto = require("crypto");
+const jwt = require("jwt");
+const secret = require("../config").secret;
+
+const UsuarioSchema = new mongoose.Schema({
+    nome: {
+        type: String,
+        required: [true,"Não pode ficar vazio."]
+    },
+    email: {
+        type: String,
+        lowercase: true,
+        unique: true,
+        required: [true, "Não pode fiar vzaio"],
+        index: true,
+        match: [/\S+@\S+\.\S+/ , "É inválido."]
+    },
+    loja: {
+        type: Schema.Types.ObjectId,
+        ref: "Loja",
+        required: [true, "Loja Não pode fiar vzaio"],
+    },
+    permissao: {
+        type: Array,
+        default: ["clientes"]
+    },
+    hash: String,
+    salt: String,
+    recovery: {
+        type: {
+            token: String,
+            date: Date
+        },
+        default: {}
+    }
+},{timestamps: true});
+
+UsuarioSchema.plugin(uniqueValidator, { message: "Já está sendo utilizado"});
+
+UsuarioSchema.methods.setSenha = function(password){
+    this.salt = crypto.randomBytes(16).toString("hex");
+    this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, "sha512").toString("hex");
+};
+
+UsuarioSchema.methods.validarSenha = function(password){
+    const hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, "sha512").toString("hex");
+    return hash === this.hash;
+};
+
+UsuarioSchema.methods.gerarToken = function(){
+    const hoje = new Date();
+    const exp = new Date(today);
+    exp.setDate(today.getDate()+15);
+
+    return jwt.sign({
+        id: this._id,
+        email: this.email,
+        nome: this.nome,
+        exp: parseFloat(exp.getTime() / 1000, 10)
+    }, secret)
+};
+
+UsuarioSchema.methods.enviarAuthJSON = function(){
+    return {
+        nome: this.nome,
+        email: this.email,
+        loja: this.loja,
+        role: this.permissao,
+        token: this.gerarToken()
+     };
+};
+
+//RECUPERAÇÃO SENHA
+UsuarioSchema.methods.criarTokenRecuperacaoSenha = function(){
+    this.recovery = {}
+    this.recovery.token = crypto.randomBytes(16).toString("hex");
+    this.recovery.date = new Date( new Date().getTime() + 24*60*60*1000);
+    return this.recovery;
+}
+
+UsuarioSchema.methods.finalizarTokenRecuperacaoSenha = function(){
+    this.recovery = { token: null, date: null};
+    return this.recovery;
+};
+
+module.exports = mongoose.model("Usuario", UsuarioSchema)
