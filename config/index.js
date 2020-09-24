3470e07e8ec50b42dc2a634615c273e02b21897f
Alan Vítor Paixão Almeida <vitorpaixaoa.loubi@gmail.com>
2020-09-24 14:03:57 -0300

    Index de links

--- config/index.js
+++ config/index.js
@@ -1,4 +1,5 @@
 module.exports = {
-    secret: ProcessingInstruction.env.NODE_ENV
-
+    secret: process.env.NODE_ENV === "production"? process.env.SECRET : "ASDHQKWDHUIASHDIU21UHDW12871EBWDBAS112EI7H12E12E",
+    api: process.env.NODE_ENV === "production" ? "http://api.lojateste.ampliee.com" : "http://localhost:3000",
+    loja: process.env.NODE_ENV === "production"? "http://lojateste.ampliee.com" : "http://localhost:8000"
 }
