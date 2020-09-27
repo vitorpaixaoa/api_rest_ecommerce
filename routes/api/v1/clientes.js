0b0d395db97de93b3b08af65f78ab1bd3ebc44b9
Alan Vítor Paixão Almeida <vitorpaixaoa.loubi@gmail.com>
2020-09-27 12:23:11 -0300

    rotas de clientes estabelecidas

---
+++ routes/api/v1/clientes.js
@@ -0,0 +1,28 @@
+const router = require("express").Router();
+
+const ClienteController = require("../../../controllers/ClienteController");
+const { LojaValidation } = require("../../../controllers/validacoes/lojaValidation")
+const { ClienteValidation } = require("../../../controllers/validacoes/clienteValidation")
+const Validation = require("express-validation");
+const auth = require("../../auth");
+
+const clienteController = new ClienteController();
+
+//AMDMIN
+router.get("/", auth.required, LojaValidation.admin, clienteController.index); //mostrar todos os clientes
+//router.get("/search/:search/pedidos", auth.required, LojaValidation.admin, clienteController.searchPedidos); //procurar um pedido em especifico
+router.get("/search/:search", auth.required, LojaValidation.admin, clienteController.search); //procurar um cliente em especifico
+router.get("/admin/:id", auth.required, LojaValidation.admin, clienteController.showAdmin); //mostrar todos os clientes
+//router.get("/admin/:id/:pedidos", auth.required, LojaValidation.admin, clienteController.showPedidos);//mostrar todos os pedidos
+
+router.put("/admin/:id", auth.required, LojaValidation.admin, clienteController.updateAdmin); //atualziar dados do cliente
+
+//CLIENTE
+
+router.get("/", auth.required, clienteController.show)
+
+router.post("/", clienteController.store)
+router.put("/:id", auth.required, clienteController.update)
+router.delete("/", auth.required, clienteController.remove)
+
+module.exports = router;
--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 
 router.use("/usuarios", require("./usuarios"));
 router.use("/lojas", require("./lojas"));
+router.use("/clientes", require("./clientes"));
 
 module.exports  = router ;
