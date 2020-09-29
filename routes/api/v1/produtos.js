ee98b209759066acd7222e66ea48a3724bbc77f0
Alan Vítor Paixão Almeida <vitorpaixaoa.loubi@gmail.com>
2020-09-29 17:43:21 -0300

    validações e rotas completas

--- routes/api/v1/produtos.js
+++ routes/api/v1/produtos.js
@@ -3,6 +3,8 @@ const router = require("express").Router();
 const ProdutoController = require("../../../controllers/ProdutoController");
 
 const { LojaValidation } = require("../../../controllers/validacoes/lojaValidation");
+const Validation = require("express-validation");
+const { ProdutoValidation } = require("../../../controllers/validacoes/produtoValidation");
 const auth = require("../../auth");
 const upload = require("../../../config/multer");
 const lojaValidation = require("../../../controllers/validacoes/lojaValidation");
@@ -11,18 +13,18 @@ const produtoController = new ProdutoController;
 /*
 ---------- ADMIN ------------ 
 */
-    router.post("/", auth.required, LojaValidation.admin, produtoController.store);
-    router.put("/:id", auth.required, LojaValidation.admin, produtoController.update);
-    router.put("/images/:id", auth.required, lojaValidation.admin, upload.array("files", 4),produtoController.updateImages);
-    router.delete("/:id", auth.required, lojaValidation.admin, produtoController.remove);
+    router.post("/", auth.required, LojaValidation.admin,Validation(ProdutoValidation.store) , produtoController.store);
+    router.put("/:id", auth.required, LojaValidation.admin, Validation(ProdutoValidation.update) , produtoController.update);
+    router.put("/images/:id", auth.required, LojaValidation.admin, Validation(ProdutoValidation.updateImages), upload.array("files", 4), produtoController.updateImages);
+    router.delete("/:id", auth.required, LojaValidation.admin, Validation(ProdutoValidation.remove) , produtoController.remove);
 
 /*
 ---------- CLIENTE/VISITANTES ----------
 */
-    router.get("/".produtoController.index);
-    router.get("/disponiveis", produtoController.indexDisponiveis);
-    router.get("/search/:search", produtoController.search);
-    router.get("/:id", produtoController.show);
+    router.get("/", Validation(ProdutoValidation.index) ,produtoController.index);
+    router.get("/disponiveis",Validation(ProdutoValidation.indexDisponiveis) , produtoController.indexDisponiveis);
+    router.get("/search/:search", Validation(ProdutoValidation.search) , produtoController.search);
+    router.get("/:id", Validation(ProdutoValidation.show) , produtoController.show);
 
 /*
 ---- VARIACOES -------
