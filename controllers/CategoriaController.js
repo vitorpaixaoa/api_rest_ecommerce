84c2e67cf236894a153df2fa3f19756821acc278
vitorpaixaoa <vitorpaixaoa.loubi@gmail.com>
2020-11-24 22:59:46 -0300

    correões

--- controllers/CategoriaController.js
+++ controllers/CategoriaController.js
@@ -24,7 +24,7 @@ class CategoriaController {
 
     //GET /:id show
     show(req,res,next){
-        Categoria.findOne({loja: req.query.loja, _id: req.params.id;})
+        Categoria.findOne({loja: req.query.loja, _id: req.params.id})
         .select("_id produtos nome disponibilidade codigo loja")
         .populate(["produtos"])
         .then(categoria => res.send({ categoria }))
